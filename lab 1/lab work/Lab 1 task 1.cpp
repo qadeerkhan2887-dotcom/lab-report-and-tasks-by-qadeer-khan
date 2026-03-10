a33115e2ec38a40808e68a6d49a642c583521c6f
qadeerkhan2887-dotcom <qadeerkhan2887@gmail.com>
2026-03-11 02:30:54 +0500

    Add files via upload

---
+++ lab 1/lab work/Lab 1 task 1.cpp
@@ -0,0 +1,17 @@
+// Name: muhammad qadeer khan
+// Program: Simple Hello World Program
+
+#include <iostream>      
+// Library used for input and output operations
+using namespace std;     
+// Allows us to use cout without writing std::
+
+int main() {             
+// Main function where program execution starts
+    
+    cout << "Hello, World!"; 
+	 // Displays the message on the screen
+    
+    return 0;            
+	// Indicates that the program finished successfully
+}
